3e407d0029b96194c896423e9a502ff2fc9ec365
mmt3d <78088314+mmt3d@users.noreply.github.com>
2026-07-19 20:51:01 +0900

    docs: 翻訳辞書の追加・調整

--- CM3D2 Converter/__init__.py
+++ CM3D2 Converter/__init__.py
@@ -4,7 +4,7 @@
 bl_info = {
     'name': "CM3D2 Converter",
     'author': '@saidenka_cm3d2, @trzrz, @luvoid, @mmt3d',
-    'version': (2026, 7, 2),
+    'version': (2026, 7, 19),
     'blender': (3, 3, 0),
     'location': "ファイル > インポート/エクスポート > CM3D2 Model (.model)",
     'description': "カスタムメイド3D2/カスタムオーダーメイド3D2専用ファイルのインポート/エクスポートを行います",
